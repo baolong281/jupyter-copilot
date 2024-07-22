6bb6c6e3c865e99d5e67a644b2b807d0ddeca723
dylan h <baolong281@gmail.com>
2024-07-21 23:07:23 -0500

    throttling

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,14 @@ class CopilotInlineProvider implements IInlineCompletionProvider {
   readonly name = 'GitHub Copilot';
   readonly identifier = 'jupyter_copilot:provider';
   notebookClients: Map<string, NotebookLSPClient>;
+  private lastRequestTime: number = 0;
+  private timeout: any = null;
+  private lastResolved: (
+    value:
+      | IInlineCompletionList<IInlineCompletionItem>
+      | PromiseLike<IInlineCompletionList<IInlineCompletionItem>>
+  ) => void = () => {};
+  private requestInProgress: boolean = false;
 
   constructor(notebookClients: Map<string, NotebookLSPClient>) {
     this.notebookClients = notebookClients;
@@ -30,19 +38,58 @@ class CopilotInlineProvider implements IInlineCompletionProvider {
     request: CompletionHandler.IRequest,
     context: IInlineCompletionContext
   ): Promise<IInlineCompletionList<IInlineCompletionItem>> {
-    console.log('Fetching completions');
+    const now = Date.now();
 
+    // debounce mechanism
+    // if a request is made within 90ms of the last request, throttle the request
+    // but if it is the last request, then make the request
+    console.log('time since last request', now - this.lastRequestTime);
+    if (this.requestInProgress || now - this.lastRequestTime < 90) {
+      console.log('THROTTLING');
+      this.lastRequestTime = now;
+
+      // this request was made less than 90ms after the last request
+      // so we resolve the last request with an empty list then clear the timeout
+      this.lastResolved({ items: [] });
+      clearTimeout(this.timeout);
+
+      return new Promise(resolve => {
+        this.lastResolved = resolve;
+        // set a timeout that will resolve the request after 200ms
+        // if no calls are made within 90ms then this will resolve and fetch
+        // if a call comes in < 90ms then this will be cleared and the request will be solved to empty list
+        this.timeout = setTimeout(async () => {
+          console.log('RESOLVING REQUEST AFTER THROTTLE');
+          this.requestInProgress = true;
+          this.lastRequestTime = Date.now();
+
+          const items = await this.fetchCompletion(request, context);
+
+          resolve(items);
+        }, 200);
+      });
+    } else {
+      // if request is not throttled, just get normally
+      console.log('NORMAL REQUEST');
+      this.requestInProgress = true;
+      this.lastRequestTime = now;
+
+      return await this.fetchCompletion(request, context);
+    }
+  }
+
+  // logic to actually fetch the completion
+  private async fetchCompletion(
+    request: CompletionHandler.IRequest,
+    context: IInlineCompletionContext
+  ): Promise<IInlineCompletionList<IInlineCompletionItem>> {
     const editor = (context as any).editor as CodeEditor.IEditor;
     const cell = (context.widget as any)._content._activeCellIndex;
     const client = this.notebookClients.get((context.widget as any).id);
-
     const cursor = editor?.getCursorPosition();
     const { line, column } = cursor;
-
     client?.sendUpdateLSPVersion();
-
     const items: IInlineCompletionItem[] = [];
-
     const completions = await client?.getCopilotCompletion(cell, line, column);
     completions?.forEach(completion => {
       items.push({
@@ -50,9 +97,7 @@ class CopilotInlineProvider implements IInlineCompletionProvider {
         isIncomplete: false
       });
     });
-
-    console.log('Completions:', items);
-
+    this.requestInProgress = false;
     return { items };
   }
 }
@@ -97,7 +142,6 @@ const plugin: JupyterFrontEndPlugin<void> = {
       execute: () => {
         // get id of current notebook panel
         const notebookPanelId = notebookTracker.currentWidget?.id;
-        console.log('ID of current notebook panel:', notebookPanelId);
         providerManager.inline?.accept(notebookPanelId || '');
       }
     });
